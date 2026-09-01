5e07408426ee3a2b65e520f106f05d6b8c2d304c
Justin Gordon <justin@shakacode.com>
2026-09-01 09:40:00 -1000

    Update setup-python lint pin expectation

--- bin/lint-test.rb
+++ bin/lint-test.rb
@@ -164,7 +164,7 @@ class LintCommandTest < Minitest::Test
     assert_includes workflow, "actions/checkout@d23441a48e516b6c34aea4fa41551a30e30af803"
     assert_includes workflow, "ruby/setup-ruby@95ef2b042f9d7a56d8268cba8559e2842e2ad01b"
     assert_includes workflow, "actions/setup-node@249970729cb0ef3589644e2896645e5dc5ba9c38"
-    assert_includes workflow, "actions/setup-python@ece7cb06caefa5fff74198d8649806c4678c61a1"
+    assert_includes workflow, "actions/setup-python@5fda3b95a4ea91299a34e894583c3862153e4b97"
   end
 
   def test_validation_and_contributor_docs_expose_the_lint_command
