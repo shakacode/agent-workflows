177e8d5ac9ce8908844e448b5f5453451a5ea97e
Justin Gordon <justin@shakacode.com>
2026-09-12 12:51:05 -1000

    Preserve seam doctor helper visibility

--- bin/agent_doctor/init_command_shell.rb
+++ bin/agent_doctor/init_command_shell.rb
@@ -9,5 +9,21 @@ module AgentWorkflowSeamDoctor
     include InitCommandShellLexical
     include InitCommandShellSafety
     include InitCommandShellState
+
+    PUBLIC_METHODS = %i[
+      active_dollar_expansion?
+      active_outer_command_substitution?
+      ansi_c_byte_for_syntax
+      ansi_c_decoded_shell_word
+      bash_outer_argument_state_expansion_at?
+      decode_ansi_c_segment
+      named_parameter_expansion_at?
+      positional_argument_expansion_at?
+      positional_state_reference?
+      shell_c_placeholder_word?
+      shell_comment_index
+      shell_word_spans
+      unquoted_shell_boundary?
+    ].freeze
   end
 end
